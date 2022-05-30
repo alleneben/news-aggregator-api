ab77605959ef932d933f2613f8acfc3f0c0adaa3
Allen Eben <allen.eben@fjorgedigital.com>
2022-05-30 05:55:07 +0000

    :refactored main file

--- main.py
+++ main.py
@@ -1,44 +1,7 @@
-from fastapi import FastAPI
-import configparser
-from datasource.DataSource import DataSource
-
-# Read environment variables from cfg file
-config_parser = configparser.ConfigParser()
-config_parser.read("cfg.ini")
+import uvicorn
 
 
-# instantiate FastAPI
-app = FastAPI()
 
-# Sources for fetching data
-sources = [
-    { 
-        "name":"reddit",
-        "url": config_parser["configurations"]["REDDIT_URL"],
-        "headers":{
-            "User-Agent": "Chrome",
-            "Content-Type": "application/json"
-        } 
-    }, 
-    {
-        "name":"newsapi",
-        "url": config_parser["configurations"]["NEWSAPI_URL"], 
-        "headers":{}
-    }
-]
-
-# Instantiate DataSource class
-data_source = DataSource(config_parser["configurations"], sources)
-
-
-# Create a get http method with FastAPI instance
-# TODO: Secure this route
-@app.get("/news")
-def get_news(query: str = None):
-    try:
-        # Call the fetch_data method passing the search word
-        data = data_source.fetch_data(f"{query}")
-        return data
-
-    except Exception as e:
-        return { "error": str(e) }
+# Check for the __main__ variable and run the app in app/api
+if __name__ == "__main__":
+    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
